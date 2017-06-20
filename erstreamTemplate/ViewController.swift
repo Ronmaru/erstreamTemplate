fe72f4229025bb923f8aaa5aa28374e361f35caa
ismail <ismail@ismails-MacBook-Pro.local>
2017-06-20 12:40:45 +0300

    Initial Commit

---
+++ erstreamTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  erstreamTemplate
+//
+//  Created by ismail on 20/06/2017.
+//  Copyright © 2017 ismail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
